a55b7073dc49ba76b49813c34f7109d12a3dfc57
Ingo <ikuba@intension.de>
2020-03-16 11:44:50 +0100

    8: Add airport finder.

--- src/main/java/featherkraken/flights/boundary/FlightResource.java
+++ src/main/java/featherkraken/flights/boundary/FlightResource.java
@@ -2,12 +2,12 @@ package featherkraken.flights.boundary;
 
 import static featherkraken.flights.error.ErrorUtil.NO_ENTITY;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.Response.Status.OK;
 
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -26,12 +26,13 @@ public class FlightResource
     public static final String PATH = "flights";
 
     @POST
+    @Consumes(APPLICATION_JSON)
     @Produces(APPLICATION_JSON)
     public Response search(SearchRequest request)
     {
         if (request == null) {
             throw new BadRequestException(NO_ENTITY);
         }
-        return Response.status(OK).entity(new GenericEntity<List<Trip>>(FlightSearcher.search(request)) {}).build();
+        return Response.ok(new GenericEntity<List<Trip>>(FlightSearcher.search(request)) {}).build();
     }
 }
---
+++ src/test/java/featherkraken/airports/boundary/AirportResourceIT.java
@@ -0,0 +1,78 @@
+package featherkraken.airports.boundary;
+
+import static featherkraken.airports.boundary.AirportResource.PATH;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyArray;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.Matchers.not;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import featherkraken.flights.entity.Airport;
+import featherkraken.flights.test.JerseyResourceProvider;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@ExtendWith(JerseyResourceProvider.class)
+public class AirportResourceIT
+{
+
+    private JerseyResourceProvider featherkraken;
+
+    @Test
+    public void should_return_valid_airports()
+    {
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("query", "FRA");
+        Response response = featherkraken.doGet(PATH, queryParams);
+
+        assertThat(response.getStatus(), equalTo(OK.getStatusCode()));
+        Airport[] airports = response.readEntity(Airport[].class);
+        assertThat(airports.length, greaterThanOrEqualTo(1));
+        assertThat(airports.length, lessThanOrEqualTo(10));
+        Airport airport = airports[0];
+        assertThat(airport.getName(), not(emptyOrNullString()));
+        assertThat(airport.getDisplayName(), not(emptyOrNullString()));
+    }
+
+    @Test
+    public void should_return_empty_list_for_unknown_query()
+    {
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("query", "isadjoasidjas9j");
+        Response response = featherkraken.doGet(PATH, queryParams);
+
+        assertThat(response.getStatus(), equalTo(OK.getStatusCode()));
+        Airport[] airports = response.readEntity(Airport[].class);
+        assertThat(airports, emptyArray());
+    }
+
+    @Test
+    public void should_return_error_for_missing_query()
+    {
+        Response response = featherkraken.doGet(PATH);
+
+        assertThat(response.getStatus(), equalTo(BAD_REQUEST.getStatusCode()));
+    }
+
+    @Test
+    public void should_return_error_for_short_query()
+    {
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("query", "a");
+        Response response = featherkraken.doGet(PATH, queryParams);
+
+        assertThat(response.getStatus(), equalTo(BAD_REQUEST.getStatusCode()));
+    }
+}
