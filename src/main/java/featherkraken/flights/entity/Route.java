ce8e921b76efb46623b1905bda1028f5155873a1
Ingo <ikuba@intension.de>
2020-06-05 14:42:57 +0200

    35: Add classType to Route.

--- src/main/java/featherkraken/flights/entity/Route.java
+++ src/main/java/featherkraken/flights/entity/Route.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import featherkraken.flights.entity.SearchRequest.ClassType;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -12,15 +13,17 @@ import lombok.experimental.Accessors;
 public class Route
 {
 
-    private Airport source;
+    private Airport   source;
 
-    private Airport target;
-
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private Date    departure;
+    private Airport   target;
 
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private Date    arrival;
+    private Date      departure;
 
-    private String  airline;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private Date      arrival;
+
+    private String    airline;
+
+    private ClassType classType;
 }
